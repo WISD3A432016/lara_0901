d4ab0dd62c42f8a8dcd6c39509d0ac82392140ae
Jing-Ru Sun <sun.3a432016@gmail.com>
2017-11-20 16:08:07 +0800

    Exercise5-1: 新增資料 -> 將表單送過來的資料用 Model 寫入資料庫

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -15,6 +15,11 @@ class AdminPostsController extends Controller
         return view('admin.posts.index', $data);
     }
 
+    public function store(Request $request)
+    {
+        Post::create($request->all());
+    }
+
     public function create()
     {
         return view('admin.posts.create');
